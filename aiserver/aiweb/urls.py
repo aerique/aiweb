bfce15f591d7a579c16fc7023b8b5ccd44b4d6c7
Jude Hungerford <edward.h@gmx.net>
2016-01-23 19:26:26 +1100

    upload form working

--- aiserver/aiweb/urls.py
+++ aiserver/aiweb/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^profile', views.profile, name='profile'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/(?P<status>[a-z_]+)/$', views.profile, name='profile'),
 ]
--- aiserver/aiweb/views.py
+++ aiserver/aiweb/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
+from django import forms
+from django.template.context_processors import csrf
+from django.shortcuts import render_to_response
+
+import os
 # Create your views here.
 
 
@@ -18,8 +24,35 @@ def index(request):
 
 
 
-def profile(request):
-    template = loader.get_template('aiweb_templates/profile.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+#def profile(request):
+#    template = loader.get_template('aiweb_templates/profile.html')
+#    context = {}
+#    return HttpResponse(template.render(context, request))
 
+class UploadFileForm(forms.Form):
+    file  = forms.FileField()
+
+def handle_uploaded_file(ffile, user):
+    path = ("uploads/" + user.username)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    with open(path + "/" + ffile.name, 'wb+') as destination:
+        for chunk in ffile.chunks():
+            destination.write(chunk)
+
+def profile(request, status="normal"):
+    if request.method == 'POST':
+        a=request.POST
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'], request.user)
+            return HttpResponseRedirect('/aiweb/profile/upload_success/')
+    else:
+        form = UploadFileForm()
+        upload_success = (status == "upload_success")
+        c = {'form': form, 
+             'user': request.user, 
+             'upload_success': upload_success}
+        c.update(csrf(request))
+        return render_to_response('aiweb_templates/profile.html', c)
