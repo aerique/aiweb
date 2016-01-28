d1ede11e1f221cf6b13dc10b76c70fd912dd36e5
Jude Hungerford <edward.h@gmx.net>
2016-01-28 22:54:37 +1100

    add model for submissions to webserver

--- web/aiweb/models.py
+++ web/aiweb/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Submission(models.Model):
+	username = models.CharField (max_length = 32)
+	game_id = models.CharField (max_length = 32)
+	timestamp = models.CharField (max_length = 32)
+	submission_id = models.CharField (max_length = 100)
+	status = models.CharField (max_length = 32)
+	report = models.CharField (max_length = 5000)
+	
+
+
