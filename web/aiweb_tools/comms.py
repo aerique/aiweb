7de2262c95477dbf37d8ce883836f75782406603
Jude Hungerford <edward.h@gmx.net>
2016-01-30 15:08:29 +1100

    refactor comms

--- web/aiweb_tools/comms.py
+++ web/aiweb_tools/comms.py
@@ -2,24 +2,22 @@ import subprocess
 
 from aiweb_tools import config
 
-def send_file_datastore_ready(filepath, target):
-	subprocess.call(["scp", filepath, config.datastore_username + "@" 
-			+ config.datastore_ip
-			+ "://" + target])
+def send_file(filepath, dest):
+	subprocess.call(["scp", filepath, dest])
+
+def send_file_ready(filepath, dest):
+	send_file(filepath, dest)
 	subprocess.call(["touch", filepath + ".ready"])
-	subprocess.call(["scp", filepath + ".ready", 
-			config.datastore_username + "@" + config.datastore_ip
-			+ "://" + target])
+	send_file(filepath + ".ready", dest)
 	subprocess.call(["rm", filepath + ".ready"])
 
-#FIXME refactor
+def send_file_datastore_ready(filepath, target):
+	send_file_ready(filepath, config.datastore_username + "@" 
+			+ config.datastore_ip
+			+ "://" + target)
+
 def send_file_webserver_ready(filepath, target):
-	subprocess.call(["scp", filepath, config.webserver_username + "@" 
+	send_file_ready(filepath, config.webserver_username + "@" 
 			+ config.webserver_ip
-			+ "://" + target])
-	subprocess.call(["touch", filepath + ".ready"])
-	subprocess.call(["scp", filepath + ".ready", 
-			config.webserver_username + "@" + config.webserver_ip
-			+ "://" + target])
-	subprocess.call(["rm", filepath + ".ready"])
+			+ "://" + target)
 
