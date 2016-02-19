c4d1d206885b306848be8266539888211f579df9
Jude Hungerford <edward.h@gmx.net>
2016-02-19 18:56:36 +1100

    update config.py

--- web/aiweb_tools/config.py
+++ web/aiweb_tools/config.py
@@ -6,7 +6,6 @@ prefix = "/home/" + username + "/aiweb/"
 
 task_ip = "127.0.0.1"
 task_port = 22
-task_username = username
 task_path = prefix + "tasks/"
 task_worker_path = prefix + "workers/"
 
@@ -17,17 +16,15 @@ datastore_submission_path = prefix + "submissions/"
 
 webserver_ip = "127.0.0.1"
 webserver_port = 22
-webserver_username = username
 webserver_results_path = prefix + "results/"
  
 matchmaker_ip = "127.0.0.1"
 matchmaker_port = 22
-matchmaker_username = username
 matchmaker_path = prefix + "matchmaker/"
 
 map_path = prefix + "maps/" # maps stored on matchmaker server
 
-isolate_bin = prefix + "web/aiweb_tools/isolate/isolate"
+isolate_bin = prefix + "isolate/isolate"
 worker_compiled = prefix + "compiled/"
 runner_working = prefix + "runner/"
 runner_prefix = "aiweb"
@@ -36,6 +33,8 @@ bin_dir = "/usr/bin/"
 
 sleeptime = 0.5
 delimiter = "____" # FIXME filter for this in usernames!
+
+results_limit = 25
 
 #temp_dir = "temp/"
 
